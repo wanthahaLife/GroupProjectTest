c35ee9492d6cf014849877c6d31d4396c807fc34
HDM <endressi15@gmail.com>
2024-04-04 13:42:54 +0900

    아이스메이커 애니메이션 진행예정
    각 얼음별로 클래스에서 따로 처리

--- GroupProJCopyBOTW/Assets/Scripts/Skill_Scripts/Skill/IceMaker.cs
+++ GroupProJCopyBOTW/Assets/Scripts/Skill_Scripts/Skill/IceMaker.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,16 +18,19 @@ public class IceMaker : Skill
 
     Vector3 iceSize = Vector3.zero;
 
-    Vector3 waterCheckBoxHalfSize = Vector3.zero;
+    //Vector3 waterCheckBoxSize = Vector3.zero;
 
-    const float WaterCheckBoxHeight = 0.1f;
+    const float WaterCheckBoxHeight = 0.05f;
 
     bool isPossible = false;
+
+    BoxCollider waterCheckCollider;
 
     protected override void Awake()
     {
         base.Awake();
         Transform ices = transform.GetChild(1);
+
 
         usingIceQueue = new Queue<Transform>(ices.childCount);
         readyIceQueue = new Queue<Transform>(ices.childCount);
@@ -37,10 +41,14 @@ public class IceMaker : Skill
         
         preview = transform.GetChild(2);
 
-        iceSize = preview.GetChild(0).lossyScale;
+        iceSize = preview.GetChild(0).lossyScale * 0.5f;
 
-        waterCheckBoxHalfSize = iceSize * 0.5f;
-        waterCheckBoxHalfSize.y = WaterCheckBoxHeight;
+        Vector3 waterCheckBoxSize = iceSize;
+        waterCheckBoxSize.y = WaterCheckBoxHeight;
+
+        waterCheckCollider = GetComponent<BoxCollider>();
+        waterCheckCollider.size = waterCheckBoxSize;
+        waterCheckCollider.enabled = false;
     }
     protected override void Start()
     {
@@ -60,6 +68,8 @@ public class IceMaker : Skill
         StopAllCoroutines();
         StartCoroutine(CreateCheck());
     }
+
+
     IEnumerator CreateCheck()
     {
         while (true)
@@ -71,31 +81,17 @@ public class IceMaker : Skill
             if (Physics.Raycast(ray, out RaycastHit hit, iceMakerDistance))
             {
                 // 물의 범위가 얼음 크기만큼 되는지 체크
-                Collider[] colliders = Physics.OverlapBox(hit.point, waterCheckBoxHalfSize);
-                foreach (Collider collider in colliders)
+                if(hit.transform.gameObject.layer == LayerMask.NameToLayer("Water"))
                 {
-                    if (collider.gameObject.layer == LayerMask.NameToLayer("Water"))
-                    {
-                        Vector3 point = (hit.point + Vector3.up * preview.lossyScale.y * 0.5f);
-                        if (!Physics.CheckBox(point, preview.lossyScale * 0.5f, Quaternion.identity, LayerMask.GetMask("Ground")))
-                        {
-                            isPossible = true;
-                            createPosition = hit.point;
-                            preview.gameObject.SetActive(true);
-                            preview.position = createPosition;
-                        }
-                        else
-                        {
-                            preview.gameObject.SetActive(false);
-                            //Debug.Log("생성 불가");
-                        }
-                    }
-                    else
-                    {
-                        preview.gameObject.SetActive(false);
-                        //Debug.Log("물 범위가 좁음");
-                    }
+                    //waterCheckCollider.
                 }
+                if(!Physics.CheckBox(hit.point, iceSize*0.5f, Quaternion.identity, LayerMask.GetMask("Ground")))
+                    {
+                        isPossible = true;
+                        createPosition = hit.point;
+                        preview.gameObject.SetActive(true);
+                        preview.position = createPosition;
+                    }
             }
             else
             {
@@ -134,7 +130,7 @@ public class IceMaker : Skill
             Handles.DrawLine(vec, hit.point, 2);
             if(hit.transform.gameObject.layer == LayerMask.NameToLayer("Water"))
             {
-                Handles.DrawWireCube(hit.point, waterCheckBoxHalfSize * 2);
+                //Handles.DrawWireCube(hit.point, waterCheckBoxSize * 2);
             }
         }
 
